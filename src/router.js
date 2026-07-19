45a48ba5c6ed21880e7e70757ad9b932f2a27edc
jackeybharthi <jackey.bharthi@gmail.com>
2026-07-20 00:30:31 +0530

    Update router.js

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Index from './components/index.vue'
 import Admin from './components/Admin.vue'
 
@@ -16,7 +16,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
